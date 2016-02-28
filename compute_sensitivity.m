0ceb0feba6a18f368bfa4671f112fea615289f22
David N. Kraemer <kraemerd17@grinnell.edu>
2016-02-27 20:01:54 -0600

    Comments.

--- compute_sensitivity.m
+++ compute_sensitivity.m
@@ -1,10 +1,13 @@
 syms x y;
 syms r1 r2 a1 a2 K1 K2;
 
+% model differential equations
 eqn1 = r1 - (2 * r1)/K1 * x - a1 * y - a2 * y == 0;
 eqn2 = r2 - (2 * r2)/K2 * y - a2 * x - a1 * x == 0;
 
 [A, B] = equationsToMatrix([eqn1, eqn2], [x, y]);
+
+% solution of the model ODEs
 X = linsolve(A,B);
 
 vars = [r1 r2 K1 K2 a1 a2];
@@ -15,7 +18,10 @@ float_unused = [];
 
 sensitivity = cell(6,1);
 
+% compute the sensitivity functions with respect to each of the model parameters
+% sensitivity{i} is the sensitivity functions of x,y with respect to vars(i)
 for i = 1:6
+    % hold one parameter as a variable, and plug in the oter five
     for j = 1:6
        if j ~= i
            unused = [unused vars(j)];
@@ -23,8 +29,12 @@ for i = 1:6
        end
     end
     
+    % compute the functions dx/dt, dy/dt with respect to the held parameter
+    % and then compute the sensitivities
     f(vars(i)) = subs(X, unused, float_unused);
     sensitivity{i} = diff(f, vars(i)) * vars(i) ./ f(vars(i));
+
+    % reset and repeat!
     unused = [];
     float_unused = [];
 end
